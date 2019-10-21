4d66207d0f561c6fa88726abef26dee07fbc39ea
Byrnes, Kristi M <kristi.m.byrnes@accenture.com>
2019-10-21 15:25:27 -0400

    Add routes to main page

---
+++ src/Routes.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Route, Switch } from "react-router-dom";
+import Home from "./containers/Home";
+
+const Routes = () => {
+	return ( 
+		<Switch>
+			<Route path="/" exact component={Home} />
+		</Switch>
+	 );
+}
+ 
+export default Routes;
---
+++ src/containers/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Home.css";
+
+const Home = () => {
+	return ( 
+		<div className="Home">
+			<div className="lander">
+				<h1>Scratch</h1>
+				<p>A simple note taking app</p>
+			</div>
+		</div>
+	 );
+}
+ 
+export default Home;
