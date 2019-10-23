722e11a6e2ac26df801ee72c28a08eef873205d2
Byrnes, Kristi M <kristi.m.byrnes@accenture.com>
2019-10-23 10:08:09 -0400

    Handle routing and sign in auth with cognito user

--- src/Routes.js
+++ src/Routes.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import NotFound from './containers/NotFound';
+import Login from './containers/Login';
+import AppliedRoute from './components/AppliedRoute';
 
-const Routes = () => {
+const Routes = ({appProps}) => {
 	return ( 
 		<Switch>
-			<Route path="/" exact component={Home} />
+			<AppliedRoute path="/" exact component={Home} appProps={appProps}/>
+			<AppliedRoute path="/login" exact component={Login} appProps={appProps}/>
 			<Route component={NotFound} />
 		</Switch>
 	 );
---
+++ src/containers/Login.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import { Auth } from "aws-amplify";
+import "./Login.css";
+
+
+
+const Login = (props) => {
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+
+	function validateForm () {
+		return email.length > 0 && password.length > 0;
+	}
+
+	async function handleSubmit (event) {
+		event.preventDefault();
+		try {
+			await Auth.signIn(email, password);
+			props.userHasAuthenticated(true);
+		} catch (e) {
+			alert(e.message);
+		}
+	}
+	return (
+		<div className="Login">
+			<form onSubmit={handleSubmit}>
+				<FormGroup controlId="email" bsSize="large">
+					<ControlLabel>Email</ControlLabel>
+					<FormControl
+						autoFocus
+						type="email"
+						value={email}
+						onChange={e => setEmail(e.target.value)}
+					/>
+				</FormGroup>
+				<FormGroup controlId="password" bsSize="large">
+					<ControlLabel>Password</ControlLabel>
+					<FormControl
+						value={password}
+						onChange={e => setPassword(e.target.value)}
+						type="password"
+					/>
+				</FormGroup>
+				<Button block bsSize="large" disabled={!validateForm()} type="submit">
+					Login
+			</Button>
+			</form>
+		</div>  );
+}
+ 
+export default Login;
+
